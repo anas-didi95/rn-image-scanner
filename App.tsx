08f8505f2a6860efbb565d628f2e15db87e10c5a
Anas Juwaidi <anas.didi95@gmail.com>
2021-02-19 12:02:21 +0800

    Amend back handler to close camera and prompt alert for exit app

--- App.tsx
+++ App.tsx
@@ -1,34 +1,63 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useFocusEffect} from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
 import AboutScreen from './src/screens/AboutScreen';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Icon} from 'native-base';
-import CameraProvider from './src/utils/contexts/CameraContext';
+import CameraProvider, {
+  useCameraContext,
+} from './src/utils/contexts/CameraContext';
 import Camera from './src/components/Camera';
 import {createStackNavigator} from '@react-navigation/stack';
+import {Alert, BackHandler} from 'react-native';
 
 const HomeTabNavigator = createBottomTabNavigator();
-const HomeTab = () => (
-  <HomeTabNavigator.Navigator initialRouteName="Home">
-    <HomeTabNavigator.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{
-        tabBarIcon: ({focused}) =>
-          focused ? <Icon name="home" /> : <Icon name="home-outline" />,
-      }}
-    />
-    <HomeTabNavigator.Screen
-      name="About"
-      component={AboutScreen}
-      options={{
-        tabBarIcon: ({focused}) =>
-          focused ? <Icon name="person" /> : <Icon name="person-outline" />,
-      }}
-    />
-  </HomeTabNavigator.Navigator>
-);
+const HomeTab = () => {
+  const cameraContext = useCameraContext();
+
+  useFocusEffect(() => {
+    cameraContext.setClose();
+    const backAction = () => {
+      Alert.alert('Hold on!', 'Are you sure you want to go exit?', [
+        {
+          text: 'Cancel',
+          onPress: () => null,
+          style: 'cancel',
+        },
+        {text: 'YES', onPress: () => BackHandler.exitApp()},
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  });
+
+  return (
+    <HomeTabNavigator.Navigator initialRouteName="Home">
+      <HomeTabNavigator.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused}) =>
+            focused ? <Icon name="home" /> : <Icon name="home-outline" />,
+        }}
+      />
+      <HomeTabNavigator.Screen
+        name="About"
+        component={AboutScreen}
+        options={{
+          tabBarIcon: ({focused}) =>
+            focused ? <Icon name="person" /> : <Icon name="person-outline" />,
+        }}
+      />
+    </HomeTabNavigator.Navigator>
+  );
+};
 
 const AppStackNavigator = createStackNavigator();
 const App = () => (
