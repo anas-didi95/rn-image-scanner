89771ac291f4c65b38403434b3813131ead2dfb9
Anas Juwaidi <anas.didi95@gmail.com>
2021-02-24 16:23:25 +0800

    Add regex to clean and test phone value

--- src/utils/hooks/useValidate.ts
+++ src/utils/hooks/useValidate.ts
@@ -1,6 +1,9 @@
 const useValidate = () => {
+  const phoneRemoveCharRegex = /[-\s]/g;
+  const phoneRegex = /^\+?[\d]{10,11}$/;
+
   const isPhoneNumber = (phone: string): boolean => {
-    if (phone) {
+    if (phoneRegex.test(phone.replace(phoneRemoveCharRegex, ''))) {
       return true;
     }
 
