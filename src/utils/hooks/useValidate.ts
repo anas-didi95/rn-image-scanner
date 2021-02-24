83e57cda7d386bddb27ad2ba2074547c47ac4583
Anas Juwaidi <anas.didi95@gmail.com>
2021-02-24 20:30:27 +0800

    Add function to check email

--- src/utils/hooks/useValidate.ts
+++ src/utils/hooks/useValidate.ts
@@ -1,6 +1,8 @@
 const useValidate = () => {
   const phoneRemoveCharRegex = /[-\s]/g;
   const phoneRegex = /^\+?[\d]{10,11}$/;
+  const emailRemoveCharRegex = /[\s]/g;
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const isPhoneNumber = (phone: string): boolean => {
     if (phoneRegex.test(phone.replace(phoneRemoveCharRegex, ''))) {
@@ -10,15 +12,25 @@ const useValidate = () => {
     return false;
   };
 
+  const isEmail = (email: string): boolean => {
+    if (emailRegex.test(email.replace(emailRemoveCharRegex, ''))) {
+      return true;
+    }
+
+    return false;
+  };
+
   const getType = (value: string): string => {
     if (isPhoneNumber(value)) {
       return 'Phone';
+    } else if (isEmail(value)) {
+      return 'Email';
     }
 
     return 'Text';
   };
 
-  return {isPhoneNumber, getType};
+  return {isPhoneNumber, isEmail, getType};
 };
 
 export default useValidate;
