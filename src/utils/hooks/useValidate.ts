45a1be2b4c1b762cf6e03a1f373f96d042a015f9
Anas Juwaidi <anas.didi95@gmail.com>
2021-02-25 15:57:13 +0800

    Add regex to check web link

--- src/utils/hooks/useValidate.ts
+++ src/utils/hooks/useValidate.ts
@@ -3,6 +3,8 @@ const useValidate = () => {
   const phoneRegex = /^\+?[\d]{10,11}$/;
   const emailRemoveCharRegex = /[\s]/g;
   const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const webLinkRemoveCharRegex = /[\s]/g;
+  const webLinkRegex = /^https?:\/\/([\w\d\\-]+\.)+\w{2,}(\/.+)?$/;
 
   const isPhoneNumber = (phone: string): boolean => {
     return phoneRegex.test(phone.replace(phoneRemoveCharRegex, ''));
@@ -12,11 +14,17 @@ const useValidate = () => {
     return emailRegex.test(email.replace(emailRemoveCharRegex, ''));
   };
 
+  const isWebLink = (webLink: string): boolean => {
+    return webLinkRegex.test(webLink.replace(webLinkRemoveCharRegex, ''));
+  };
+
   const getType = (value: string): string => {
     if (isPhoneNumber(value)) {
       return 'Phone';
     } else if (isEmail(value)) {
       return 'Email';
+    } else if (isWebLink(value)) {
+      return 'Web Link';
     }
 
     return 'Text';
